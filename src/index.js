3bdc5294c719aaf0e486848cc9bc67693a796c04
Saipriti980 <saipriti052@gmail.com>
2024-08-05 19:11:18 +0530

    New Change

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <App />
-      </Router>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
